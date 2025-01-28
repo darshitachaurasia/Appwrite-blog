937099c141b51898b6cae0ef5b7fce2965862f08
darshitachaurasia <161128989+darshitachaurasia@users.noreply.github.com>
2025-01-28 14:33:44 +0530

    File name changes

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,8 +1,8 @@
 const conf = {
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwritedatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID), // Fix: Database ID should map to DATABASE_ID
-    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID), // Fix: Collection ID should map to COLLECTION_ID
+    appwritedatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID), 
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID), 
     appwritebucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 };
 
